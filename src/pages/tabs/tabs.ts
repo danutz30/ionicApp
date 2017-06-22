298ce21d607784971abc735377e3760f525853dd
Danut Anca <danyanca2002@gmail.com>
2017-06-22 16:38:15 +0300

    testtest

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,22 @@
+import { LoginPage } from './../login/login';
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { HelpPage } from './../help/help';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
+  tab4Root = HelpPage;
+
+  constructor() {
+
+  }
+}
